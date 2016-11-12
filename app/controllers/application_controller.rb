40d9f9e270519e5e3c9fc29799dd96dc3dede45b
Ryosuke Kawauchi <ryosukekawauchi@kawauchiryouyuu-no-MacBook-Air.local>
2016-11-12 11:51:55 +0900

    Add nickname to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configue_permitted_parameters, if: :devise_controller?
+
+  def configue_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
