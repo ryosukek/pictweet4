c2776b3605fbc312cb239d532633d80fec8b3ef0
Ryosuke Kawauchi <ryosukekawauchi@kawauchiryouyuu-no-MacBook-Air.local>
2016-11-15 12:07:28 +0900

    finish complete pictweet4

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'                       # ルートパスの設定
-  get 'tweets'          => 'tweets#index'   # ツイート一覧画面
-  get 'tweets/new'      => 'tweets#new'     # ツイート投稿画面
-  post 'tweets'         => 'tweets#create'  # ツイート保存
-  delete 'tweets/:id'   => 'tweets#destroy' # ツイート削除
-  patch 'tweets/:id'    => 'tweets#update'  # ツイート編集後更新
-  get 'tweets/:id/edit' => 'tweets#edit'    # ツイート編集
-  get 'tweets/:id'      => 'tweets#show'    # ツイート詳細画面
-  get 'users/:id'       => 'users#show'     # Mypage一覧画面
+  # get 'tweets'          => 'tweets#index'   # ツイート一覧画面
+  # get 'tweets/new'      => 'tweets#new'     # ツイート投稿画面
+  # post 'tweets'         => 'tweets#create'  # ツイート保存
+  # delete 'tweets/:id'   => 'tweets#destroy' # ツイート削除
+  # patch 'tweets/:id'    => 'tweets#update'  # ツイート編集後更新
+  # get 'tweets/:id/edit' => 'tweets#edit'    # ツイート編集
+  # get 'tweets/:id'      => 'tweets#show'    # ツイート詳細画面
+  # get 'users/:id'       => 'users#show'     # Mypage一覧画面
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
